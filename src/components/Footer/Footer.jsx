acb5b4a303fee29107adce9c5ddc8d500746e7a1
hajirajuma <hajiraj475@gmail.com>
2025-07-16 03:32:34 -0700

    updating project

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
              </div>
              <hr/>
              <div className="footer-bottom">
-                  <p className="footer-bottom-left">&copy Hajeerah Juma.All rights reserved.</p> 
+                  <p className="footer-bottom-left">&copy; Hajeerah Juma.All rights reserved.</p> 
                   <div className="footer-bottom-right">
                     <p>Term of Services</p>
                     <p>Privacy Policy</p>
